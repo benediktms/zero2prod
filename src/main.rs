5affbaced50f9973a31f3220afbf59fd1c1de65b
Benedikt Schnatterbeck <benediktms@hey.com>
2022-07-27 16:57:52 +0100

    Use Tcp listener to spin up application

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+
+    println!("Listening on port {}", port);
+
+    run(listener)?.await
 }
