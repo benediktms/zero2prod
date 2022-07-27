b9384f93184ae307fcc9d516c5640568fe3a2d39
Benedikt Schnatterbeck <benediktms@hey.com>
2022-07-27 15:21:17 +0100

    Implement inital healthcheck endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod prelude {
+    pub use actix_web::*;
+    pub use tokio::*;
+}
+
+use prelude::*;
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
