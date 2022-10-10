8f7380a32329e30677d8df65b46e84529f8c5c69
Benedikt Schnatterbeck <benediktms@hey.com>
2022-10-10 23:22:28 +0100

    Add dockerfile and imporve configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,13 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -39,12 +45,51 @@ impl DatabaseSettings {
 }
 
 pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine current directory");
+    let config_directory = base_path.join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENV")
+        .unwrap_or_else(|_| "development".into())
+        .try_into()
+        .expect("Failed to parse APP_ENV");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(config_directory.join("base.yaml")))
+        .add_source(config::File::from(
+            config_directory.join(&environment_filename),
         ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
+}
+
+pub enum Environment {
+    Development,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Development => "development",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "development" => Ok(Self::Development),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. \
+                User either 'development' or 'production'",
+                other
+            )),
+        }
+    }
 }
