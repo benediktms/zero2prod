db5bd242352ec073f704acbeb9669bdfcfbb4922
Benedikt Schnatterbeck <benediktms@hey.com>
2022-11-24 22:46:39 +0000

    Fix insert_subscriber query

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -48,13 +48,13 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, created_at)
-        VALUES ($1, $2, $3, $4)
+        INSERT INTO subscriptions (id, email, name, created_at, status)
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
-        Utc::now()
+        Utc::now(),
     )
     .execute(pool)
     .await
