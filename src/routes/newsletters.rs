6ae965cdc315795d32c5529e6bb705ce1816b42c
Benedikt Schnatterbeck <benediktms@hey.com>
2022-12-30 17:30:33 +0100

    Validate user credentials when accessing the newseltters endpoint

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -5,7 +5,7 @@ use actix_web::{
 };
 use anyhow::Context;
 use reqwest::{header, StatusCode};
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 use sqlx::PgPool;
 
@@ -69,13 +69,46 @@ struct Credentials {
     password: Secret<String>,
 }
 
+async fn validate_credentails(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id = sqlx::query!(
+        r#"
+    SELECT id
+    FROM users
+    WHERE username  = $1 AND password = $2
+    "#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Vailed to perform query to vallidate credentials")
+    .map_err(PublishError::AuthError)?;
+
+    user_id
+        .map(|row| row.id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password"))
+        .map_err(PublishError::AuthError)
+}
+
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, pool, email_client, request), fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     body: Json<BodyData>,
     pool: Data<PgPool>,
     email_client: Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+
+    let user_id = validate_credentails(credentials, &pool).await?;
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
 
     for subscriber in subscribers {
