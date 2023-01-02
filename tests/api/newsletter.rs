3ece7c5d67485adf17f672616f2df04fd5be14fb
Benedikt Schnatterbeck <benediktms@hey.com>
2023-01-02 14:43:46 +0100

    Adjust argon2 implementation to prevent timing attacks

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,4 @@
+use uuid::Uuid;
 use wiremock::{
     matchers::{any, method, path},
     Mock, ResponseTemplate,
@@ -103,6 +104,35 @@ async fn requests_missing_authorization_are_rejected() {
         .expect("Failed to execute request");
 
     assert_eq!(res.status().as_u16(), 401);
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        res.headers()["WWW-Authenticate"]
+    );
+}
+
+#[tokio::test]
+pub async fn non_existing_user_is_rejected() {
+    let app = spawn_app().await;
+    let username = Uuid::new_v4().to_string();
+    let password = Uuid::new_v4().to_string();
+
+    assert_ne!(app.test_user.password, password);
+
+    let res = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert_eq!(401, res.status().as_u16());
     assert_eq!(
         r#"Basic realm="publish""#,
         res.headers()["WWW-Authenticate"]
